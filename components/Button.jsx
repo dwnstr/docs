5789dd4e589362324f2db3501a0f078b6a4eff1f
Floh <48927090+Flohhhhh@users.noreply.github.com>
2023-05-12 00:34:42 +0000

    Button Component & ULC Getting Started Page

---
+++ components/Button.jsx
@@ -0,0 +1,24 @@
+"use client";
+
+import React from "react";
+
+const Button = ({ label, href, onClick }) => {
+  return (
+    <button
+      onClick={() => {
+        if (href) {
+          window.open(href, "_blank");
+        } else if (onClick) {
+          onClick();
+        } else {
+          console.log("No action");
+        }
+      }}
+      className="nx-border nx-border-white/50 nx-min-w-32 nx-w-fit nx-min-h-8 nx-px-4 nx-py-2 nx-bg-blue-600 nx-rounded-md nx-font-medium"
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;
--- components/Test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Test = () => {
-  return (
-    <div className="nx-bg-red-500">test component</div>
-  )
-}
-
-export default Test
